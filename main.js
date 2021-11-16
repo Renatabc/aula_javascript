1089d71e13da93e1ac68d38b720c950baa72f096
Renata Brito Costa <93830634+Renatabc@users.noreply.github.com>
2021-11-16 11:43:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+
+
+function botão(){
+    alert("Obrigada por clicar");
+}
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade>=18){
+        validar= true
+    }else{
+        validar= false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+*/
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, nome_nome){
+    return frase.replace(nome, nome_nome);
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+/*var d = new Date();
+alert(d);
+*/
+/*var count;
+for(count=1; count <= 5; count++){
+    alert(count);
+}
+*/
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+}
+*/
+/*var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade");
+}
+*/
+//var lista = ["maçã","pêra","uva"];
+//lista.push("laranja");
+//console.log(lista);
+//alert(lista[0]);
+
+//var nome = "Renata Brito";
+//var idade = 26;
+//alert(nome + " tem " + idade + " anos");
+//var n1 = 5;
+//var n2 = 10;
+//alert(n1 + n2);
